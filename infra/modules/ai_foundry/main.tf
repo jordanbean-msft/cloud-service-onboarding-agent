82c76b0e8307cd41afeffbe6f8e7d13159048988
Jordan Bean <jordanbean@microsoft.com>
2025-06-13 10:36:00 -0500

    terr

--- infra/modules/ai_foundry/main.tf
+++ infra/modules/ai_foundry/main.tf
@@ -11,7 +11,7 @@ data "azapi_resource" "resource_group" {
 
 resource "azapi_resource" "ai_foundry_account" {
   type      = "Microsoft.CognitiveServices/accounts@2025-04-01-preview"
-  name      = "afa-${module.naming.cognitive_account.name}"
+  name      = "afap-${module.naming.cognitive_account.name}"
   location  = var.location
   parent_id = data.azapi_resource.resource_group.id
   body = {
@@ -20,18 +20,21 @@ resource "azapi_resource" "ai_foundry_account" {
     }
     kind = "AIServices"
     identity = {
-      type = "UserAssigned"
-      userAssignedIdentities = {
-        "${var.user_assigned_identity_id}" = {}
-      }
+      type = "SystemAssigned"
+      # type = "UserAssigned"
+      # userAssignedIdentities = {
+      #   "${var.user_assigned_identity_id}" = {}
+      # }
     }
     properties = {
       publicNetworkAccess = var.public_network_access_enabled ? "Enabled" : "Disabled"
       networkAcls = {
         #bypass         = "AzureServices"
-        defaultAction = "Allow"
+        defaultAction       = "Allow"
+        virtualNetworkRules = []
+        ipRules             = []
       }
-      customSubDomainName    = "afa-${module.naming.cognitive_account.name}"
+      customSubDomainName    = "afap-${module.naming.cognitive_account.name}"
       disableLocalAuth       = false
       allowProjectManagement = true
       networkInjections = [{
@@ -56,92 +59,93 @@ module "avm-res-network-privateendpoint" {
   subresource_names              = ["account"]
 }
 
-resource "azapi_resource" "ai_foundry_project" {
-  type      = "Microsoft.CognitiveServices/accounts/projects@2025-04-01-preview"
-  name      = "project1"
-  location  = var.location
-  parent_id = resource.azapi_resource.ai_foundry_account.id
-  depends_on = [
-    module.avm-res-network-privateendpoint
-  ]
-  body = {
-    identity = {
-      type = "UserAssigned"
-      userAssignedIdentities = {
-        "${var.user_assigned_identity_id}" = {}
-      }
-    }
-    properties = {
-      description = "project1"
-      displayName = "project1"
-    }
-  }
-}
+# resource "azapi_resource" "ai_foundry_project" {
+#   type      = "Microsoft.CognitiveServices/accounts/projects@2025-04-01-preview"
+#   name      = "project1"
+#   location  = var.location
+#   parent_id = resource.azapi_resource.ai_foundry_account.id
+#   depends_on = [
+#     module.avm-res-network-privateendpoint
+#   ]
+#   body = {
+#     identity = {
+#       type = "SystemAssigned"
+#       # type = "UserAssigned"
+#       # userAssignedIdentities = {
+#       #   "${var.user_assigned_identity_id}" = {}
+#       # }
+#     }
+#     properties = {
+#       description = "project1"
+#       displayName = "project1"
+#     }
+#   }
+# }
 
-resource "azapi_resource" "ai_foundry_project_connection_cosmos_db_account" {
-  type      = "Microsoft.CognitiveServices/accounts/projects/connections@2025-04-01-preview"
-  name      = "cosmosdb"
-  parent_id = resource.azapi_resource.ai_foundry_project.id
-  body = {
-    properties = {
-      category = "CosmosDB"
-      target   = var.cosmos_db_account_document_endpoint
-      authType = "AAD"
-      metadata = {
-        ApiType    = "Azure"
-        ResourceId = var.cosmos_db_account_resource_id
-        location   = var.location
-      }
-    }
-  }
-}
+# resource "azapi_resource" "ai_foundry_project_connection_cosmos_db_account" {
+#   type      = "Microsoft.CognitiveServices/accounts/projects/connections@2025-04-01-preview"
+#   name      = "cosmosdb"
+#   parent_id = resource.azapi_resource.ai_foundry_project.id
+#   body = {
+#     properties = {
+#       category = "CosmosDB"
+#       target   = var.cosmos_db_account_document_endpoint
+#       authType = "AAD"
+#       metadata = {
+#         ApiType    = "Azure"
+#         ResourceId = var.cosmos_db_account_resource_id
+#         location   = var.location
+#       }
+#     }
+#   }
+# }
 
-resource "azapi_resource" "ai_foundry_project_connection_storage_account" {
-  type      = "Microsoft.CognitiveServices/accounts/projects/connections@2025-04-01-preview"
-  name      = "storage"
-  parent_id = resource.azapi_resource.ai_foundry_project.id
-  body = {
-    properties = {
-      category = "Storage"
-      target   = var.storage_account_resource_id
-      authType = "AAD"
-      metadata = {
-        ApiType    = "Azure"
-        ResourceId = var.storage_account_resource_id
-        location   = var.location
-      }
-    }
-  }
-}
+# resource "azapi_resource" "ai_foundry_project_connection_storage_account" {
+#   type      = "Microsoft.CognitiveServices/accounts/projects/connections@2025-04-01-preview"
+#   name      = "storage"
+#   parent_id = resource.azapi_resource.ai_foundry_project.id
+#   body = {
+#     properties = {
+#       category = "Storage"
+#       target   = var.storage_account_resource_id
+#       authType = "AAD"
+#       metadata = {
+#         ApiType    = "Azure"
+#         ResourceId = var.storage_account_resource_id
+#         location   = var.location
+#       }
+#     }
+#   }
+# }
 
-resource "azapi_resource" "ai_foundry_project_connection_ai_search" {
-  type      = "Microsoft.CognitiveServices/accounts/projects/connections@2025-04-01-preview"
-  name      = "search"
-  parent_id = resource.azapi_resource.ai_foundry_project.id
-  body = {
-    properties = {
-      category = "Search"
-      target   = var.ai_search_resource_id
-      authType = "AAD"
-      metadata = {
-        ApiType    = "Azure"
-        ResourceId = var.ai_search_resource_id
-        location   = var.location
-      }
-    }
-  }
-}
+# resource "azapi_resource" "ai_foundry_project_connection_ai_search" {
+#   type      = "Microsoft.CognitiveServices/accounts/projects/connections@2025-04-01-preview"
+#   name      = "search"
+#   parent_id = resource.azapi_resource.ai_foundry_project.id
+#   body = {
+#     properties = {
+#       category = "Search"
+#       target   = var.ai_search_resource_id
+#       authType = "AAD"
+#       metadata = {
+#         ApiType    = "Azure"
+#         ResourceId = var.ai_search_resource_id
+#         location   = var.location
+#       }
+#     }
+#   }
+# }
 
-resource "azapi_resource" "ai_foundry_project_capability_hosts" {
-  type      = "Microsoft.CognitiveServices/accounts/projects/capabilityHosts@2025-04-01-preview"
-  name      = "project-capability-host"
-  parent_id = resource.azapi_resource.ai_foundry_project.id
-  body = {
-    properties = {
-      capabilityHostKind       = "Agents"
-      vectorStoreConnections   = [resource.azapi_resource.ai_foundry_project_connection_ai_search.name]
-      storageConnections       = [resource.azapi_resource.ai_foundry_project_connection_storage_account.name]
-      threadStorageConnections = [resource.azapi_resource.ai_foundry_project_connection_cosmos_db_account.name]
-    }
-  }
-}
+# resource "azapi_resource" "ai_foundry_project_capability_hosts" {
+#   type      = "Microsoft.CognitiveServices/accounts/projects/capabilityHosts@2025-04-01-preview"
+#   name      = "project-capability-host"
+#   parent_id = resource.azapi_resource.ai_foundry_project.id
+#   body = {
+#     properties = {
+#       capabilityHostKind       = "Agents"
+#       vectorStoreConnections   = [resource.azapi_resource.ai_foundry_project_connection_ai_search.name]
+#       storageConnections       = [resource.azapi_resource.ai_foundry_project_connection_storage_account.name]
+#       threadStorageConnections = [resource.azapi_resource.ai_foundry_project_connection_cosmos_db_account.name]
+#     }
+#   }
+# }
